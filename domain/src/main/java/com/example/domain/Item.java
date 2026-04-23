34bc4dab08fcf2c85a3a60c80b1e0a20538befb6
Benjamin Muschko <benjamin.muschko@gmail.com>
2026-04-23 09:32:56 -0600

    Remove unused validation annotations and spring-boot-starter-validation dependency

--- domain/src/main/java/com/example/domain/Item.java
+++ domain/src/main/java/com/example/domain/Item.java
@@ -5,8 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
@@ -17,11 +15,9 @@ public class Item {
 
     private Long id;
 
-    @NotBlank
     private String name;
 
     private String description;
 
-    @NotNull
     private BigDecimal price;
 }
